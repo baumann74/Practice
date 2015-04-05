9512f896755e2e9a858bc29990a61e36c190ff2e
AndersBaumann <baumann74@gmail.com>
2015-04-06 00:46:35 +0300

    Added first Project Euler problem.

---
+++ ProjectEuler.Test/ProblemsTests.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ProjectEuler.Test
+{
+	using NUnit.Framework;
+
+	[TestFixture]
+	class ProblemsTests
+	{
+		[Test]
+		public void Largest_palindrome_product()
+		{
+			Assert.AreEqual(906609, Problems.Largest_palindrome_product());
+		}
+	}
+}
---
+++ ProjectEuler/Problems.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace ProjectEuler
+{
+	public class Problems
+	{
+
+		public static long Largest_palindrome_product()
+		{
+			return 
+				(from x in Enumerable.Range(100, 900)
+				from y in Enumerable.Range(100, 900)
+				where Is_palindrome(x * y)
+				select x * y)
+				.Max();
+		}
+
+		private static bool Is_palindrome(long n)
+		{
+			var r = long.Parse(n.ToString().Reverse().Aggregate("", (a, c) => (a + c)));
+			return r == n;
+		}
+	}
+}
