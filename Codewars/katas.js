c47e9008ee142555e08cea17e14499ff4dc23dd6
AndersBaumann <baumann74@gmail.com>
2015-05-19 00:04:10 +0300

    Added first JavaScript Codewar katas.

---
+++ Codewars/katas.js
@@ -0,0 +1,45 @@
+﻿// Collatz Conjecture Length
+// http://www.codewars.com/kata/54fb963d3fe32351f2000102/train/javascript
+
+function collatz(n) {
+	var result = 1;
+	while (n != 1) {
+		if (n % 2 === 0) {
+			n = n / 2;
+			result++;
+		} else {
+			n = (n * 3) + 1;
+			result++;
+		}
+	}
+	return result;
+}
+
+describe("Collatz Conjecture Length", function () {
+
+	it("Tests", function () {
+		Test.assertEquals(collatz(20), 8);
+		Test.assertEquals(collatz(15), 18);
+	});
+});
+
+// ********************************************************
+// Jaden Casing Strings
+//http://www.codewars.com/kata/5390bac347d09b7da40006f6/train/javascript
+
+String.prototype.toJadenCase = function () {
+	var list = this.split(' ').map(function (v) {
+		return v.charAt(0).toUpperCase() + v.slice(1);
+	});
+	return list.join(" ");
+};
+
+describe("Jaden Casing Strings", function () {
+
+	it("Tests", function () {
+		var str = "How can mirrors be real if our eyes aren't real";
+		Test.assertEquals(str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't Real");
+	});
+});
+
+
