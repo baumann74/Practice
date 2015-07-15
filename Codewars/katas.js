bba3545252709ad6dcf8ad9c6a6ceb9c60159401
AndersBaumann <baumann74@gmail.com>
2015-07-15 22:25:20 +0300

    Katas#Mines works.

--- Codewars/katas.js
+++ Codewars/katas.js
@@ -647,7 +647,7 @@ function solve(map, miner, exit) {
 function solveHelper(map, x, y, exit, visited_map, rute) {
 	if (x === exit.x && y === exit.y) return true;
 
-	if (!map[0][y] || !map[x] || !map[x][y] || visited_map[x][y]) return false;
+	if (map[0][y] === undefined || !map[x] || !map[x][y] || visited_map[x][y]) return false;
 
 	visited_map[x][y] = true;
 
@@ -671,48 +671,48 @@ function solveHelper(map, x, y, exit, visited_map, rute) {
 }
 
 
-//describe('A trivial map (1x1)', function () {
-//	var map = [[true]];
-//
-//	it('Should return an empty array, since we\'re already at the goal', function () {
-//		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 0, y: 0 }), []);
-//	});
-//});
-//
-//describe('A pretty simple map (2x2)', function () {
-//	var map = [[true, false],
-//	  [true, true]];
-//
-//	it('Should return the only correct move', function () {
-//		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 1, y: 0 }), ['right']);
-//	});
-//
-//	it('Should return the only moves necessary', function () {
-//		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 1, y: 1 }), ['right', 'down']);
-//	});
-//});
-//
-//describe('A linear map(1x4)', function () {
-//	var map = [[true], [true], [true], [true]];
-//
-//	it('Should return a chain of moves to the right', function () {
-//		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 3, y: 0 }), ['right', 'right', 'right']);
-//	});
-//
-//	it('Should return a chain of moves to the left', function () {
-//		Test.assertSimilar(solve(map, { x: 3, y: 0 }, { x: 0, y: 0 }), ['left', 'left', 'left']);
-//	});
-//});
-//
-//describe('Should walk around an obstacle (3x3 map)', function () {
-//	var map = [[true, true, true],
-//	[false, false, true],
-//	[true, true, true]];
-//
-//	it('Should return the right sequence of moves', function () {
-//		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 2, y: 0 }), ['down', 'down', 'right', 'right', 'up', 'up']);
-//	});
-//});
+describe('A trivial map (1x1)', function () {
+	var map = [[true]];
+
+	it('Should return an empty array, since we\'re already at the goal', function () {
+		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 0, y: 0 }), []);
+	});
+});
+
+describe('A pretty simple map (2x2)', function () {
+	var map = [[true, false],
+	  [true, true]];
+
+	it('Should return the only correct move', function () {
+		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 1, y: 0 }), ['right']);
+	});
+
+	it('Should return the only moves necessary', function () {
+		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 1, y: 1 }), ['right', 'down']);
+	});
+});
+
+describe('A linear map(1x4)', function () {
+	var map = [[true], [true], [true], [true]];
+
+	it('Should return a chain of moves to the right', function () {
+		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 3, y: 0 }), ['right', 'right', 'right']);
+	});
+
+	it('Should return a chain of moves to the left', function () {
+		Test.assertSimilar(solve(map, { x: 3, y: 0 }, { x: 0, y: 0 }), ['left', 'left', 'left']);
+	});
+});
+
+describe('Should walk around an obstacle (3x3 map)', function () {
+	var map = [[true, true, true],
+	[false, false, true],
+	[true, true, true]];
+
+	it('Should return the right sequence of moves', function () {
+		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 2, y: 0 }), ['down', 'down', 'right', 'right', 'up', 'up']);
+	});
+});
 
 describe('Should be able to change directions multiple times (5x5 map)', function () {
 	var map = [[true, true, false, false, false],
@@ -726,15 +726,15 @@ describe('Should be able to change directions multiple times (5x5 map)', functio
 		  ['down', 'right', 'down', 'right', 'down', 'right', 'down', 'right']);
 	});
 });
-//
-//describe('Should avoid dead-ends (5x5 map)', function () {
-//	var map = [[true, true, true, false, true],
-//	  [false, false, true, false, true],
-//	  [true, true, true, true, true],
-//	  [true, false, true, false, false],
-//	  [false, true, true, true, true]];
-//
-//	it('Should return the right moves', function () {
-//		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 4, y: 4 }), ['down', 'down', 'right', 'right', 'right', 'right', 'down', 'down'])
-//	});
-//});
+
+describe('Should avoid dead-ends (5x5 map)', function () {
+	var map = [[true, true, true, false, true],
+	  [false, false, true, false, true],
+	  [true, true, true, true, true],
+	  [true, false, true, false, false],
+	  [false, true, true, true, true]];
+
+	it('Should return the right moves', function () {
+		Test.assertSimilar(solve(map, { x: 0, y: 0 }, { x: 4, y: 4 }), ['down', 'down', 'right', 'right', 'right', 'right', 'down', 'down'])
+	});
+});
