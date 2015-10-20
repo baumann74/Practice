8a732212638fb9e3dd73af27bf2ceff1e8b1b0b7
baumann74 <baumann74@gmail.com>
2015-10-20 10:51:37 +0300

    Codewars#romanNumeralEncoder,

--- Codewars/katas.js
+++ Codewars/katas.js
@@ -1251,3 +1251,39 @@ Test.describe('Simple Tests', function () {
 		Test.assertEquals(firstNonRepeatingLetter('abcdabcd'), '');
 	});
 });
+
+
+// ********************************************************
+// http://www.codewars.com/kata/51b62bf6a9c58071c600001b/train/csharp
+// Roman Numerals Encoder.
+
+function romanNumeralEncoder(number) {
+	var map = {
+		1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX",
+		10: "X", 20: "XX", 30: "XIX", 40: "XL", 50: "L", 60: "LX", 70: "LXX", 80: "LXXX", 90: "XC",
+		100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC", 800: "DCCC", 900: "CM",
+		1000: "M", 2000: "MM"
+	};
+
+	return number.toString().split('').reverse()
+		.map(function (x, index) {
+			return x * Math.pow(10, index);
+		})
+		.filter(function (y) {
+			return y > 0;
+		})
+		.reverse()
+		.map(function (m) {
+			return map[m];
+		})
+		.join('');
+}
+
+Test.assertEquals(romanNumeralEncoder(1), "I");
+Test.assertEquals(romanNumeralEncoder(2), "II");
+Test.assertEquals(romanNumeralEncoder(4), "IV");
+Test.assertEquals(romanNumeralEncoder(707), "DCCVII");
+Test.assertEquals(romanNumeralEncoder(1990), "MCMXC");
+Test.assertEquals(romanNumeralEncoder(2008), "MMVIII");
+Test.assertEquals(romanNumeralEncoder(1666), "MDCLXVI");
+
